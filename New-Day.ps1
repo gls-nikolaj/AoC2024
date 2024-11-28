e92e1cd67aa4852573dafa231b41098469067194
Nikolaj <Nikolaj.Schomacker@gls-denmark.com>
2024-11-28 14:14:43 +0100

    ready :)

--- New-Day.ps1
+++ New-Day.ps1
@@ -1,4 +1,4 @@
-$today = Get-Date -Date "2023-12-02"
+$today = Get-Date # -Date "2023-12-02"
 
 $day = ($today).ToString("dd")
 $dayInt = $today.Day
@@ -6,7 +6,7 @@ $yearInt = $today.Year
 
 if(!(Test-Path -Path $day -PathType Container)) {
     New-Item -ItemType Directory -Path $day
-    Copy-Item -Recurse -Filter *.c .\template\* $day
+    Copy-Item -Recurse .\template\* $day
 
     $session = Get-Content -Path "session.txt"
     Invoke-WebRequest -Uri "https://adventofcode.com/$yearInt/day/$dayInt/input" -OutFile $day\input.txt -Headers @{"Cookie" = "session=$session"}
---
+++ template/Run.ps1
@@ -0,0 +1,2 @@
+zig cc -o christmas.exe .\main.c
+.\christmas.exe
