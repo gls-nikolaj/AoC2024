f1ab37abeb991a9a34efb5715a0285236e9536eb
Nikolaj <2525536+svc-user@users.noreply.github.com>
2024-12-01 16:36:46 +0100

    add helper functions in own script

---
+++ helpers/helpers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *
+read_input() {
+    FILE *h_input = fopen("input.txt", "r");
+    fseek(h_input, 0L, SEEK_END);
+    long fs = ftell(h_input);
+    fseek(h_input, 0L, SEEK_SET);
+
+    char *f_buff = malloc(sizeof(char) * fs + 1);
+    memset(f_buff, 0, fs+1);
+
+    size_t rb = fread(f_buff, sizeof(char), fs, h_input);
+    if(rb != fs) {
+        //free(f_buff);
+        fprintf(stderr, "WARN: read %zu bytes of %ld of input.txt\n", rb, fs);
+        
+        //exit(-1);
+    }
+
+    return f_buff;
+}
+
+int 
+comp_int (const void * elem1, const void * elem2) {
+    int f = *((int*)elem1);
+    int s = *((int*)elem2);
+    if (f > s) return  1;
+    if (f < s) return -1;
+    return 0;
+}
---
+++ helpers/helpers.h
@@ -0,0 +1,7 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+char * read_input();
+int comp_int (const void * elem1, const void * elem2);
+
+#endif 
