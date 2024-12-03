451fef728b6f04f79b57116907cad3a1cf218317
Nikolaj <Nikolaj.Schomacker@gls-denmark.com>
2024-12-03 14:22:20 +0100

    sick code for day3 part2

--- 03/main.c
+++ 03/main.c
@@ -72,10 +72,6 @@ void part_1(char *input)
         fflush(stdout);
 
         result += (num1 * num2);
-
-        // take each token and validate
-        // if number => add to buff_1
-        // if comma => atoi(buff_1)
     }
 
     // 187705165 = too low
@@ -85,10 +81,88 @@ void part_1(char *input)
 void part_2(char *input)
 {
     printf("Part 2:\n");
+    
+    unsigned long result = 0;
 
+    bool in_do = true;
+    char digit_1[4];
+    char digit_2[4];
+    while (1)
+    {        
+        memset(digit_1, 0, 4);
+        memset(digit_2, 0, 4);
+
+        if(!input) break; // when no more mul's
+
+        char *next_mul = strstr(input, "mul("); // find next `mul(`
+
+        char *next_do = strstr(input, "do()");
+        char *next_dont = strstr(input, "don't()");
+
+        if(next_mul > next_dont) {
+
+            if(!next_do && !next_mul) break;
+
+            // ¯\_(ツ)_/¯
+            input = next_do;
+            if(!input)
+                input = next_mul;
+            else {
+                input++;
+                continue;
+            }
+        } else {
+            input = next_mul;
+        }
+
+        // else if(in_do) {
+        //     in_do = false;
+        //     input = next_dont;
+        //     continue;
+        // }
+
+        // if(!in_do) {
+        //     input = next_do;
+        //     in_do = true;
+        //     continue;
+        // }
+
+
+        if(!input) break; // when no more mul's
+
+        printf("%.13s => ", input);
+        fflush(stdout);
+        
+        input += 4; // skip `mul(`
+
+        int i = 0;
+        while(is_num(*input) && i < 3) {
+            digit_1[i++] = *input;
+
+            input++;
+        }
+
+        if(*input++ != ',') continue; // invalid instruction
+
+        int j = 0;
+        while(is_num(*input) && j < 3) {
+            digit_2[j++] = *input;
+
+            input++;
+        }
+
+        if(*input != ')') continue; // invalid instruction
+        
+        int num1 = atoi(digit_1);
+        int num2 = atoi(digit_2);
+
+        printf("% 3d * % 3d => % 6d\n", num1, num2, num1 * num2);
+        fflush(stdout);
+
+        result += (num1 * num2);
+    }
     
     
-
-    int result = 0;
-    printf("Result is: %d\n", result);
+    // 109634158 = too low
+    printf("Result is: %lu\n", result);
 }
