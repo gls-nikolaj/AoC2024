32b748039d3d1dc504a85d3a7e05254463d7b4df
Nikolaj <Nikolaj.Schomacker@gls-denmark.com>
2024-12-03 11:10:18 +0100

    Add bool, true and false

--- 03/main.c
+++ 03/main.c
@@ -20,7 +20,7 @@ int main()
     return 0;
 }
 
-int 
+bool 
 is_num(char c)
 {
     return c >= '0' && c <= '9';
--- helpers/helpers.h
+++ helpers/helpers.h
@@ -1,6 +1,10 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
+#define bool short
+#define true 1
+#define false 0
+
 char * read_input(const char *filename);
 int comp_int (const void * elem1, const void * elem2);
 
